baa1852dcb3ad2d9bceab27bab9947842d72cfb5
quan <thanhquan.ng@gmail.com>
2017-02-18 07:57:23 +0700

    Able to go to Contact Us and Menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'contact_us' => 'home/contact_us' #For hiding the 'home'
+
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
