40bb499499eab2fadd9b170a309f169fb1de29ab
quan <thanhquan.ng@gmail.com>
2017-02-19 06:55:40 +0700

    Add menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+if Section.count == 0
+  %w(Breakfast Lunch Dinner Drinks).each do |name|
+    Section.create!(name: name)
+  end
+else
+  puts 'Data existed'
+end
+
