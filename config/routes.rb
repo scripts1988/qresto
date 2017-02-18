e4a5a0e9f48195cf529d4f6227c31653f90266f7
quan <thanhquan.ng@gmail.com>
2017-02-18 23:58:17 +0700

    Address and GoogleMap adding

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'contact_us' => 'home/contact_us' #For hiding the 'home'
+  get 'contact_us' => 'home#contact_us'
 
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
